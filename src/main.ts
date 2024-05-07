8d93764b827e1d2b5819ee0aa7fafec92b9d9559
Cristian Costescu <cristian.costescu@icloud.com>
2024-05-07 20:34:23 +0300

    Color problem

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,20 @@ import 'zone.js';
   standalone: true,
   template: `
     <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    
+    <div class="square">
+      <p>This square should have background.
+    </div>
   `,
+  styles: `
+    @use "./UI.scss" as UI;
+ 
+    :host .square{
+      aspect-ratio: 1/1;
+      width: 100px;
+      background-color: UI.getColor(primary, .25)
+    }
+  `
 })
 export class App {
   name = 'Angular';
